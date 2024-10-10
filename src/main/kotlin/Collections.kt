bdcb1dba90340e75ce17e870fa94ab0d83a8e2f0
Albert <albertsantosr@gmail.com>
2024-10-10 04:20:58 -0500

    Flow controls

--- src/main/kotlin/Collections.kt
+++ src/main/kotlin/Collections.kt
@@ -1,42 +1,42 @@
 fun main() {
-    val list: List<Note> = listOf(
-        Note("Title 1", "Description 1"),
-        Note("Title 2", "Description 2"),
-        Note("Title 5", "Description 5"),
-        Note("Title 4", "Description 4"),
-        Note("Title 3", "Description 3")
+    val list: List<Note3> = listOf(
+        Note3("Title 1", "Description 1"),
+        Note3("Title 2", "Description 2"),
+        Note3("Title 5", "Description 5"),
+        Note3("Title 4", "Description 4"),
+        Note3("Title 3", "Description 3")
     )
 
-    val emptyList: List<Note> = emptyList()
+    val emptyList: List<Note3> = emptyList()
 
-    val mutableList: MutableList<Note> = mutableListOf(
-        Note("Title 6", "Description 6"),
-        Note("Title 7", "Description 7")
+    val mutableList: MutableList<Note3> = mutableListOf(
+        Note3("Title 6", "Description 6"),
+        Note3("Title 7", "Description 7")
     )
-    mutableList.add(Note("Title 8", "Description 8"))
+    mutableList.add(Note3("Title 8", "Description 8"))
 
     val setList = setOf(
-        Note("Title 9", "Description 9"),
-        Note("Title 9", "Description 9"),
-        Note("Title 10", "Description 10"),
-        Note("Title 9", "Description 9"),
-        Note("Title 10", "Description 10")
+        Note3("Title 9", "Description 9"),
+        Note3("Title 9", "Description 9"),
+        Note3("Title 10", "Description 10"),
+        Note3("Title 9", "Description 9"),
+        Note3("Title 10", "Description 10")
     )
 
     val map = mapOf(
-        Pair(1, Note("Title 1", "Description 1")),
-        Pair(2, Note("Title 3", "Description 3")),
-        Pair(3, Note("Title 4", "Description 4")),
-        Pair(4, Note("Title 7", "Description 7")),
-        Pair(5, Note("Title 0", "Description 0"))
+        Pair(1, Note3("Title 1", "Description 1")),
+        Pair(2, Note3("Title 3", "Description 3")),
+        Pair(3, Note3("Title 4", "Description 4")),
+        Pair(4, Note3("Title 7", "Description 7")),
+        Pair(5, Note3("Title 0", "Description 0"))
     )
 
     val map2 = mapOf(
-        1 to Note("Title 1", "Description 1"),
-        2 to Note("Title 3", "Description 3"),
-        3 to Note("Title 4", "Description 4"),
-        4 to Note("Title 7", "Description 7"),
-        5 to Note("Title 0", "Description 0")
+        1 to Note3("Title 1", "Description 1"),
+        2 to Note3("Title 3", "Description 3"),
+        3 to Note3("Title 4", "Description 4"),
+        4 to Note3("Title 7", "Description 7"),
+        5 to Note3("Title 0", "Description 0")
     )
 
     val result = list
